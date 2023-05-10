085c8dfc34a7588368c073ae5e784170a765490f
k3tw13z3l <pascal@it.vu.nl>
2023-05-10 16:22:23 +0200

    start with develop

---
+++ js/armyUnit.js
@@ -0,0 +1,25 @@
+const ArmySheet_Author = "k3tw13z3l";
+const ArmySheet_Version = "0.0.1";
+const ArmySheet_LastUpdated = 1652226769; //Date.now().toString().substr(0, 10);
+
+Hooks.on("ready", function() {
+  console.log("-=> Army Sheet v" + ArmySheet_Version + " <=- [" + (new Date(ArmySheet_LastUpdated * 1000)) + "]");
+});
+
+class ArmySheet extends ActorSheet {
+	get template() {
+		return "modules/army-sheet/army-sheet.html";
+	}
+
+	static get defaultOptions() {
+		const options = super.defaultOptions;
+		options.classes.push('dnd5e army-sheet');
+		mergeObject(options, {width: 750, height: 500});
+		return options;
+	}
+}
+
+Actors.registerSheet("dnd5e", ArmySheet, {
+	types: ["character"],
+	makeDefault: false
+});
