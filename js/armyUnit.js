dea0c72e6cbef813d85af8d4d49f4399a8a21de0
k3tw13z3l <pascal@it.vu.nl>
2023-05-15 23:17:52 +0200

    fixed

--- js/armyUnit.js
+++ js/armyUnit.js
@@ -1,12 +1,14 @@
 const ArmySheet_Author = "k3tw13z3l";
 const ArmySheet_Version = "0.0.1";
 const ArmySheet_LastUpdated = 1652226769; //Date.now().toString().substr(0, 10);
-const mName="armySheet"
-
-
+const mName="armySheet";
 
 Hooks.on("ready", function() {
   console.log("-=> Army Sheet v" + ArmySheet_Version + " <=- [" + (new Date(ArmySheet_LastUpdated * 1000)) + "]");
+});
+
+Hooks.on('preUpdateActor', (actor, updatedFlags) => {
+	setArmySheetDefaults(actor)
 });
 
 class ArmySheet extends ActorSheet {
@@ -17,7 +19,7 @@ class ArmySheet extends ActorSheet {
 
 	static get defaultOptions() {
 		const options = super.defaultOptions;
-		options.classes.push('armyUnit');
+		options.classes.push('dnd5e actor army-sheet');
 		mergeObject(options, {
 			width: 748,
 			height: 641
@@ -27,54 +29,52 @@ class ArmySheet extends ActorSheet {
 
   async getData(options) {
 		const data = await super.getData(options);
-
-		const DEFAULT_UNIT_DATA = {
-			'army': {
-				'type': "[type]",
-				'ancestry': "[ancestry]",
-				'equipment': "[equipment]",
-				'experience': "[experience]",
-				'commander': "[commander]",
-				'tier': "I",
-				'damage': "1",
-				'numberOfAtk': 1,
-				'special': {
-					diminishable: 1
-				},
-				'traits': [],
-				'stats': {
-					'attack': {
-						value: null,
-						bonus: 0,
-						advantage: 0,
-						disadvantage: 0
+		data.isGM = game.user.isGM;
+		if (!data.actor.flags[mName]) {
+	  	data.actor.flags[mName] = {
+		  	'army': {
+			  	'type': "[type]",
+				  'ancestry': "[ancestry]",
+			  	'equipment': "[equipment]",
+		  		'experience': "[experience]",
+			  	'commander': "[commander]",
+			   	'tier': "I",
+				 	'damage': "1",
+					'numberOfAtk': 1,
+					'special': {
+						diminishable: 1
 					},
-					'defense': {
-						value: null,
-						bonus: 0,
-						advantage: 0,
-						disadvantage: 0
-					},
-					'power': {
-						value: null,
-						bonus: 0,
-						advantage: 0,
-						disadvantage: 0
-					},
-					'morale': {
-						value: null,
-						bonus: 0,
+					'traits': [],
+					'stats' : {
+						'attack': {
+							value: null,
+							bonus: 0,
+							advantage: 0,
+							disadvantage: 0
+						},
+						'defense': {
+							value: null,
+							bonus: 0,
+							advantage: 0,
+							disadvantage: 0
+						},
+						'power': {
+							value: null,
+							bonus: 0,
+							advantage: 0,
+							disadvantage: 0
+						},
+						'morale': {
+							value: null,
+							bonus: 0,
+						}
 					}
 				}
 			}
 		}
 
-		data.isGM = game.user.isGM;
-		// if (!data.actor.flags[mName]) {
-		data.army = DEFAULT_UNIT_DATA;
-
 	  for (const item of data.items) {
-			data.army.traits.push({
+			data.actor.flags[mName].army.traits.push({
 				id: item._id,
 				name: item.name,
 				activation: item.data?.activation?.type ||'none',
@@ -100,3 +100,11 @@ Actors.registerSheet("dnd5e", ArmySheet, {
 	types: ["character"],
 	makeDefault: false
 });
+
+
+export function setArmySheetDefaults(actor) {
+	const existingArmySheetValues = actor.getFlag('armySheet', 'army') || {};
+	const mergedArmySheetValues = mergeObject(existingArmySheetValues, DEFAULT_UNIT_DATA);
+
+	actor.setFlag('armySheet', 'army', mergedArmySheetValues);
+}
