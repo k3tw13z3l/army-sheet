60f7ff92270891a127ce9c0a9abfb51f16044121
k3tw13z3l <pascal@it.vu.nl>
2023-05-19 17:32:23 +0200

    Fixed

--- js/armyUnit.js
+++ js/armyUnit.js
@@ -2,6 +2,51 @@ const ArmySheet_Author = "k3tw13z3l";
 const ArmySheet_Version = "0.0.0";
 const mName="armySheet";
 
+const DEFAULT_UNIT_DATA = {
+	type: "infantry",
+	ancestry: "undead",
+	equipment: "light",
+	experience: "regular",
+	commander: "Mathu'Gar",
+	tier: "I",
+	damage: 1,
+	numberOfAtk: 1,
+	special: {
+		diminishable: 1
+	},
+	traits: [],
+	stats: {
+		attack: {
+			value: null,
+			bonus: 0,
+			advantage: 0,
+			disadvantage: 0
+		},
+		defense: {
+			value: null,
+			bonus: 0,
+			advantage: 0,
+			disadvantage: 0
+		},
+		power: {
+			value: null,
+			bonus: 0,
+			advantage: 0,
+			disadvantage: 0
+		},
+		toughness: {
+      value: null,
+			bonus: 0
+		},
+		morale: {
+			value: null,
+			bonus: 0,
+		}
+	}
+};
+
+
+
 class ArmySheet extends dnd5e.applications.actor.ActorSheet5eNPC {
 
 	get template() {
@@ -22,52 +67,10 @@ class ArmySheet extends dnd5e.applications.actor.ActorSheet5eNPC {
   async getData(options) {
 		const mycontext = await super.getData(options);
 
-		mycontext.items = Array.from(this.actor.items);
-
 		mycontext.isGM = game.user.isGM;
 		// if (!mycontext.actor.flags[mName]?.army) {
 	  // 	mycontext.actor.flags[mName] = {
-		mycontext.army = duplicate(this.actor.getFlag('armySheet', 'army') ||
-		{
-			'army': {
-				'type': "infantry",
-				'ancestry': "undead",
-				'equipment': "light",
-				'experience': "regular",
-				'commander': "Mathu'Gar",
-				'tier': "I",
-				'damage': "1",
-				'numberOfAtk': 1,
-				'special': {
-					'diminishable': 1
-				},
-				'traits': [],
-				'stats' : {
-					'attack': {
-						value: null,
-						bonus: 0,
-						advantage: 0,
-						disadvantage: 0
-					},
-					'defense': {
-						value: null,
-						bonus: 0,
-						advantage: 0,
-						disadvantage: 0
-					},
-					'power': {
-						value: null,
-						bonus: 0,
-						advantage: 0,
-						disadvantage: 0
-					},
-					'morale': {
-						value: null,
-						bonus: 0,
-					}
-				}
-			}
-	  })
+		mycontext.army = duplicate(this.actor.getFlag('armySheet', 'army') || DEFAULT_UNIT_DATA);
 
 	  for (const item of mycontext.items) {
 			mycontext.actor.flags[mName]?.army?.traits.push({
