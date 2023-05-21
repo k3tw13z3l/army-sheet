9726c3e83c46553ed6aa127411b8e021be604754
k3tw13z3l <pascal@it.vu.nl>
2023-05-21 13:11:28 +0200

    Fixed

--- js/armyUnit.js
+++ js/armyUnit.js
@@ -88,7 +88,7 @@ class ArmySheet extends dnd5e.applications.actor.ActorSheet5eNPC {
 					mycontext.army.ancestry = item.name;
 					continue;
 				} else if (requirements === "Experience") {
-					mycontext.army.experiments = item.name;
+					mycontext.army.experience = item.name;
 					continue
 				} else if (requirements === "Type") {
 					mycontext.army.type = item.name;
@@ -122,7 +122,7 @@ class ArmySheet extends dnd5e.applications.actor.ActorSheet5eNPC {
 
 	_traitIsExpanded(trait) {
 		return !!trait.flags[mName]?.army_trait_expanded?.[game.user.id] ||
-						!!game.user.getFlag(mName, `army_trait_expanded.${trait._id}`);
+						!!game.user.flsgs(mName, `army_trait_expanded.${trait._id}`);
   }
 
 	async _onTraitNameClicked(evt) {
