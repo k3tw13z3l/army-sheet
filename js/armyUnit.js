a623711468a64e4ce03b397432223efe315044ec
k3tw13z3l <pascal@it.vu.nl>
2023-06-21 16:20:42 +0200

    Fixed

--- js/armyUnit.js
+++ js/armyUnit.js
@@ -74,12 +74,11 @@ class ArmySheet extends dnd5e.applications.actor.ActorSheet5eNPC {
 		const mycontext = await super.getData(options);
 
 		mycontext.isGM = game.user.isGM;
-    // if (!mycontext.actor.flags[mName]) {
-		//   mycontext.actor.flags[mName] = DEFAULT_UNIT_DATA;
-		// }
+    if (!mycontext.actor.flags[mName]) {
+		  mycontext.actor.flags[mName] = DEFAULT_UNIT_DATA;
+		}
 		console.log("mycontext 0: ", mycontext.actor.flags[mName]);
-		mycontext.army = duplicate(this.actor.getFlag(mName, 'army') || DEFAULT_UNIT_DATA);
-    console.log("mycontext 1: ", mycontext.actor.flags[mName].army);
+		console.log("mycontext 1: ", mycontext.actor.flags[mName].army);
 		mycontext.army = mycontext.actor.flags[mName].army;
 		console.log("mycontext 2: ", mycontext.army);
 
